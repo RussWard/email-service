fecd850cf9d321bf35cf3b5cbb857ea2c6bc9508
Russ Ward <maddogward@gmail.com>
2017-09-05 08:50:41 -0700

    Working on sendgrid integration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const creds = require('../config.js');
+const sgMail = require('@sendgrid/mail');
 const aws = require('aws');
 
 const IP = process.env.IP || 'localhost';
@@ -8,11 +10,22 @@ const PORT = process.env.PORT || '3030';
 
 const app = express();
 
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.post('/sendMessage', (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
+  let key = process.env.SENDGRID_API_KEY || creds.SENDGRID_API_KEY;
+  sgMail.setApiKey(key);
+  const msg = {
+    to: req.body.recipientEmail,
+    from: req.body.senderEmail,
+    subject: 'Hello From Russ email service',
+    text: req.body.message,
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
 })
 
 app.listen(PORT, () => {
